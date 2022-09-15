a3992ccd16c9b4a0f9b898818215ea1d5e4b723e
Janani2102 <2019is021@stu.ucsc.cmb.ac.lk>
2022-09-15 22:50:08 +0530

    Customer UI

--- src/screens/Customer/Cus_PostJobAD.js
+++ src/screens/Customer/Cus_PostJobAD.js
@@ -166,12 +166,20 @@ function Cus_PostJobAD({navigation}) {
                   <Picker.Item label="Painter" value="c6" />
                 </Picker>
 
-                {/* <TextInput
+                <TextInput
                   style= {styles.textinput}
                   onChangeText={handleChange('image')}
                   value={values.requireddate}
                   label="Choose Image"
-                /> */}
+                />
+
+        {/* <View style={styles.container}>
+              <Image source={{ uri: "https://i.imgur.com/TkIrScD.png" }} style={{ width: 305, height: 159 }} />
+            
+              <Text style={{color: '#888', fontSize: 18}}> 
+                To share a photo from your phone with a friend, just press the button below!
+              </Text>
+            </View>                                            */}
 
               
 
--- src/screens/Customer/Cus_RecentJob.js
+++ src/screens/Customer/Cus_RecentJob.js
@@ -14,7 +14,24 @@ import {
   Dimensions,
 } from "react-native";
 
-import { DataTable } from 'react-native-paper';
+import { Card, Title, Paragraph, Chip } from "react-native-paper";
+import { FontAwesomeIcon } from "@fortawesome/react-native-fontawesome";
+import {
+  faCalendar,
+  faChevronLeft,
+  faLocationDot,
+} from "@fortawesome/free-solid-svg-icons";
+
+import {
+  RadioButton,
+  List,
+  Checkbox,
+  Button,
+  Avatar,
+  Appbar,
+} from "react-native-paper";
+
+//import { DataTable } from 'react-native-paper';
 
 function Cus_RecentJob({navigation}) {
   return (
@@ -46,7 +63,7 @@ function Cus_RecentJob({navigation}) {
           </TouchableOpacity>
         </View>
         
-        <DataTable>
+        {/* <DataTable>
         <DataTable.Header>
           <DataTable.Title >ID</DataTable.Title>
           <DataTable.Title >Job Title</DataTable.Title>
@@ -68,8 +85,213 @@ function Cus_RecentJob({navigation}) {
        <DataTable.Cell >10/08/2022</DataTable.Cell>
      </DataTable.Row>
 
-      </DataTable>
+      </DataTable> */}
 
+      {/* ----------------------------------Adding the table as cards-------------------------------------------------------- */}
+{/* One card reprent a row in the table, starting row */}
+      <View>
+            <Card style={{ backgroundColor: "#F3F5F7", margin: 10 }}>
+              <Card.Content>
+                <View style={{ flexDirection: "row" }}>
+                  <View style={{ flexDirection: "column" }}>
+                 
+                  <Title>ID</Title>
+                   
+                    {/* <Text>Location : Seeduwa</Text> */}
+                    <View style={{ flexDirection: "row" }}>
+                      <Avatar.Image
+                        size={24}
+                        source={require("../../assests/imgs/User01.jpg")}
+                        style={{ marginRight: 10, marginLeft: 10 }}
+                      />
+                      <Text>Service Provider</Text>
+                    </View>
+                    <Chip
+                      onPress={() => console.log("Pressed location")}
+                      style={{ marginBottom: 5 }}
+                    >
+                      <View>
+                        <FontAwesomeIcon icon={faLocationDot} size={0} />
+                      </View>
+                      <Text>Location</Text>
+                    </Chip>
+                    <Chip
+                      onPress={() => console.log("Pressed date")}
+                      style={{ marginBottom: 5 }}
+                    >
+                      <View>
+                        <FontAwesomeIcon icon={faCalendar} size={0} />
+                      </View>
+                      <Text>Date the job placed</Text>
+                    </Chip>
+
+                  </View>
+                  <View style={{ flexDirection: "column", paddingLeft: 20 }}>
+                    {/* <Button
+                      style={{ marginBottom: 10 }}
+                      mode="outlined"
+                      onPress={() => setVisible(true)}
+                    >
+                      {" "}
+                      Accept{" "}
+                    </Button>
+                    <Button
+                      style={{ marginBottom: 10 }}
+                      mode="outlined"
+                      onPress={() => console.log("")}
+                    >
+                      {" "}
+                      Reject
+                    </Button> */}
+                     <Title>Job Title</Title>
+                    
+                     <Chip
+                      onPress={() => console.log("Job Status")}
+                      style={{ marginBottom: 5, backgroundColor: "grey" }}
+                    >
+                      
+                      <Text>Job Status</Text>
+                    </Chip>
+
+                    <Button
+                      mode="outlined"
+                      onPress={() => console.log("Pressed")}
+                    >
+                      {" "}
+                    Change 
+                    </Button>
+                  </View>
+                </View>
+              </Card.Content>
+            </Card>
+            </View>
+{/* -------------------------------------Ending  row -----------------------------------------------------------------------*/}
+{/* One card reprent a row in the table, starting row */}
+<View>
+            <Card style={{ backgroundColor: "#F3F5F7", margin: 10 }}>
+              <Card.Content>
+                <View style={{ flexDirection: "row" }}>
+                  <View style={{ flexDirection: "column" }}>
+                 
+                  <Title>01</Title>
+                   
+                    {/* <Text>Location : Seeduwa</Text> */}
+                    <View style={{ flexDirection: "row" }}>
+                      <Avatar.Image
+                        size={24}
+                        source={require("../../assests/imgs/User01.jpg")}
+                        style={{ marginRight: 10, marginLeft: 10 }}
+                      />
+                      <Text>Ihone Doe</Text>
+                    </View>
+                    <Chip
+                      onPress={() => console.log("Pressed location")}
+                      style={{ marginBottom: 5 }}
+                    >
+                      <View>
+                        <FontAwesomeIcon icon={faLocationDot} size={0} />
+                      </View>
+                      <Text>Mirissa</Text>
+                    </Chip>
+                    <Chip
+                      onPress={() => console.log("Pressed date")}
+                      style={{ marginBottom: 5 }}
+                    >
+                      <View>
+                        <FontAwesomeIcon icon={faCalendar} size={0} />
+                      </View>
+                      <Text>2022/07/12</Text>
+                    </Chip>
+
+                  </View>
+                  <View style={{ flexDirection: "column", paddingLeft: 20 }}>
+          
+                     <Title>Plumbing</Title>
+                    
+                     <Chip
+                      onPress={() => console.log("Job Status")}
+                      style={{ marginBottom: 5, backgroundColor: "orange" }}
+                    >
+                      
+                      <Text>Ongoing</Text>
+                    </Chip>
+
+                    <Button
+                      mode="outlined"
+                      onPress={() => console.log("Pressed")}
+                    >
+                      {" "}
+                    Change 
+                    </Button>
+                  </View>
+                </View>
+              </Card.Content>
+            </Card>
+            </View>
+{/* -------------------------------------Ending  row -----------------------------------------------------------------------*/}
+{/* One card reprent a row in the table, starting row */}
+<View>
+            <Card style={{ backgroundColor: "#F3F5F7", margin: 10 }}>
+              <Card.Content>
+                <View style={{ flexDirection: "row" }}>
+                  <View style={{ flexDirection: "column" }}>
+                 
+                  <Title>02</Title>
+                   
+                    {/* <Text>Location : Seeduwa</Text> */}
+                    <View style={{ flexDirection: "row" }}>
+                      <Avatar.Image
+                        size={24}
+                        source={require("../../assests/imgs/User01.jpg")}
+                        style={{ marginRight: 10, marginLeft: 10 }}
+                      />
+                      <Text>Larry Jone</Text>
+                    </View>
+                    <Chip
+                      onPress={() => console.log("Pressed location")}
+                      style={{ marginBottom: 5 }}
+                    >
+                      <View>
+                        <FontAwesomeIcon icon={faLocationDot} size={0} />
+                      </View>
+                      <Text>Colombo</Text>
+                    </Chip>
+                    <Chip
+                      onPress={() => console.log("Pressed date")}
+                      style={{ marginBottom: 5 }}
+                    >
+                      <View>
+                        <FontAwesomeIcon icon={faCalendar} size={0} />
+                      </View>
+                      <Text>2022/07/10</Text>
+                    </Chip>
+
+                  </View>
+                  <View style={{ flexDirection: "column", paddingLeft: 20 }}>
+          
+                     <Title>Painting</Title>
+                    
+                     <Chip
+                      onPress={() => console.log("Job Status")}
+                      style={{ marginBottom: 5, backgroundColor: "green" }}
+                    >
+                      
+                      <Text>Completed</Text>
+                    </Chip>
+
+                    <Button
+                      mode="outlined"
+                      onPress={() => console.log("Pressed")}
+                    >
+                      {" "}
+                    Change 
+                    </Button>
+                  </View>
+                </View>
+              </Card.Content>
+            </Card>
+            </View>
+{/* -------------------------------------Ending  row -----------------------------------------------------------------------*/}
         </ScrollView>
     </SafeAreaView>
 
@@ -77,3 +299,4 @@ function Cus_RecentJob({navigation}) {
 }
 
 export default Cus_RecentJob
+
