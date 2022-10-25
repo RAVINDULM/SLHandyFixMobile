4afa7928e6af8eff5b962ca3e2d2c15bc46abdb3
Janani2102 <2019is021@stu.ucsc.cmb.ac.lk>
2022-10-25 20:51:25 +0530

    edit profile done

--- src/screens/Customer/Cus_EditProfile.js
+++ src/screens/Customer/Cus_EditProfile.js
@@ -2,10 +2,7 @@ import React, { useState } from "react";
 // import axios from "axios";
 import Axios from "axios";
 
-
 // import PhotoUpload from 'react-native-photo-upload'
-
-// import PhoneInput from 'react-phone-number-input'
 
 import {
   SafeAreaView,
@@ -21,99 +18,52 @@ import {
   Dimensions,
 } from "react-native";
 
-import { Picker } from '@react-native-picker/picker';
-import { Formik } from 'formik';
-import { Button, TextInput, Appbar } from 'react-native-paper';
-import PhoneInput from 'react-native-phone-input'
-import DatePicker from 'react-native-datepicker';
-
-// exports.createEditProfile = (req, res) => {
-//   const {
-//     firstName, 
-//     lastName, 
-//     contactNo, 
-//     address, 
-//     email,
-//   } =  new CustomerModel(req.body);
-//   console.log("advertisement controller called", req.body);
-//   console.log("advertisement controller called", req.file.path);
-
-//   CustomerModel.createEditProfile(
-//     {
-//     firstName: firstName,
-//     lastName: lastName, 
-//     contactNo: contactNo, 
-//     address: address, 
-//     email:email, 
-//     },
-//     (err, customer) => {
-//       if (err){
-//         res.send(err);
-//       } else if (!customer.length){
-//         res.send("Customer advertisement send successfully!");
-//       }
-//     }
-//   )
-// }
-
+import { Picker } from "@react-native-picker/picker";
+import { Formik } from "formik";
+import { Button, TextInput, Appbar } from "react-native-paper";
+import PhoneInput from "react-native-phone-input";
+import DatePicker from "react-native-datepicker";
 
 function Cus_EditProfile({ navigation }) {
-
-  const [value, setValue] = useState()
+  const [value, setValue] = useState();
   // const [selectedValue, setSelectedValue] = useState("district");
   const [selectedCategory, setSelectedCategory] = useState("category");
 
   return (
     <SafeAreaView>
       <ScrollView style={{ padding: 20 }}>
-
         <View style={styles.content}>
-
-
-          {/* <Formik
-            initialValues={{ title: '' }}
-            // validate={values => {
-            //   let errors = {};
-            //   if (!values.email) {
-            //     errors.email = 'Required';
-            //   } else if (
-            //     !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)
-            //   ) {
-            //     errors.email = 'Invalid email address';
-            //   }
-            //   return errors;
-            // }}
-            onSubmit={values => {
-              Alert.alert(JSON.stringify(values, null, 2));
-              Keyboard.dismiss();
-            }
-            }
-          > */}
-
-                  <Formik
-                  initialValues={{ firstName: "", lastName: "", contactNo: "", email: "" }}
-                      onSubmit={(values) => {
-                          console.log(values); 
-                          Axios.put("http://10.22.163.170:5000/api/v1/customer/editProfile", {
-                          id:1,
-                          firstName: values.firstName,
-                          lastName: values.lastName, 
-                          contactNo: values.contactNo, 
-                          // address: values.address, 
-                          email: values.email, 
-                          }).then(() => {
-                          alert("successfully added!");
-                          navigate("Cus_Profile") 
-                          });
-                      }}
-                >
-
+          <Formik
+            initialValues={{
+              firstName: "",
+              lastName: "",
+              contactNo: "",
+              email: "",
+            }}
+            onSubmit={(values) => {
+              console.log(values);
+              Axios.put(
+                "http://10.22.163.170:5000/api/v1/customer/editProfile",
+                {
+                  id: 1,
+                  firstName: values.firstName,
+                  lastName: values.lastName,
+                  contactNo: values.contactNo,
+                  // address: values.address,
+                  email: values.email,
+                }
+              ).then(() => {
+                alert("successfully added!");
+                navigate("Cus_Profile");
+              });
+            }}
+          >
             {({ handleChange, handleSubmit, values, errors }) => (
               <View>
                 <TextInput
                   style={styles.textinput}
                   mode="outlined"
-                  onChangeText={handleChange('firstName')}
+                  onChangeText={handleChange("firstName")}
                   value={values.firstName}
                   label="First Name"
                   placeholder="Enter your first name..."
@@ -122,7 +72,7 @@ function Cus_EditProfile({ navigation }) {
                 <TextInput
                   mode="outlined"
                   style={styles.textinput}
-                  onChangeText={handleChange('lastName')}
+                  onChangeText={handleChange("lastName")}
                   value={values.lastName}
                   label="Last Name"
                   placeholder="Enter your last name..."
@@ -131,70 +81,51 @@ function Cus_EditProfile({ navigation }) {
                 <TextInput
                   mode="outlined"
                   style={styles.textinput}
-                  onChangeText={handleChange('contactNo')}
+                  onChangeText={handleChange("contactNo")}
                   value={values.contactNo}
                   label="Contact Number"
                   placeholder="Enter your Contact Number..."
-
                 />
-
-                {/* <PhoneInput
-                  style={styles.phoneInput}
-                  defaultCode="IN"
-                 /> */}
 
 
                 <TextInput
                   style={styles.textinput}
                   mode="outlined"
-                  onChangeText={handleChange('email')}
+                  onChangeText={handleChange("email")}
                   value={values.email}
                   label="Email"
                   placeholder="email"
                 />
-                
-
-                {/* <TextInput
-                  mode="outlined"
-                  style={styles.textinput}
-                  onChangeText={handleChange('address')}
-                  value={values.address}
-                  label="Address"
-                  placeholder="Enter your address here..."
-                /> */}
-
-                {/* <Picker
-                  selectedValue={selectedValue}
-                  style={styles.picker}
-                  onValueChange={(itemValue, itemIndex) => setSelectedValue(itemValue)}
-                >
-                  <Picker.Item label="District" value="d1" />
-                  <Picker.Item label="Matara" value="d2" />
-                  <Picker.Item label="Galle" value="d3" />
-                  <Picker.Item label="Hambantota" value="d4" />
-                  <Picker.Item label="Colombo" value="d5" />
-                </Picker> */}
 
                 <View style={styles.buttonview}>
-                  <Button onPress={handleChange} style={styles.buttonCancel} color="white">Cancel</Button>
-                  <Button onPress={handleSubmit} style={styles.buttonSubmit} color="white">Update</Button>
+                  <Button
+                    onPress={handleChange}
+                    style={styles.buttonCancel}
+                    color="white"
+                  >
+                    Cancel
+                  </Button>
+                  <Button
+                    onPress={handleSubmit}
+                    style={styles.buttonSubmit}
+                    color="white"
+                  >
+                    Update
+                  </Button>
                 </View>
-
-
               </View>
             )}
           </Formik>
         </View>
       </ScrollView>
     </SafeAreaView>
-  )
+  );
 }
-
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#ecf0f1',
+    backgroundColor: "#ecf0f1",
   },
   content: {
     padding: 16,
@@ -203,45 +134,44 @@ const styles = StyleSheet.create({
     width: 110,
     marginTop: 26,
     paddingVertical: 10,
-    backgroundColor: "#2538B8"
+    backgroundColor: "#2538B8",
   },
   buttonCancel: {
     width: 110,
     marginTop: 26,
     paddingVertical: 10,
-    backgroundColor: "silver"
+    backgroundColor: "silver",
   },
   text: {
-    textAlign: 'left',
+    textAlign: "left",
     width: 230,
     fontSize: 16,
-    color: "#000"
+    color: "#000",
   },
   phoneInput: {
-    borderColor: '#ddd',
+    borderColor: "#ddd",
     borderWidth: 2,
     borderRadius: 2,
-    padding: 16
+    padding: 16,
   },
   textinput: {
-    borderBottomColor: '#ccc',
+    borderBottomColor: "#ccc",
     backgroundColor: "#fff",
     borderBottomWidth: 1,
     paddingBottom: 3,
     marginBottom: 10,
-    
   },
-  datepicker:{
-    borderBottomColor: '#ccc',
+  datepicker: {
+    borderBottomColor: "#ccc",
     backgroundColor: "#fff",
     borderBottomWidth: 1,
     paddingBottom: 3,
     marginBottom: 10,
-    width:290,
-    height:60,
+    width: 290,
+    height: 60,
   },
   picker: {
-    borderBottomColor: '#ccc',
+    borderBottomColor: "#ccc",
     borderBottomWidth: 1,
     paddingBottom: 3,
     marginBottom: 10,
@@ -249,11 +179,11 @@ const styles = StyleSheet.create({
     width: 300,
   },
   buttonview: {
-    display: 'flex',
-    flexDirection: 'row',
+    display: "flex",
+    flexDirection: "row",
     // width:50,
-    justifyContent: 'space-around',
-  }
-})
+    justifyContent: "space-around",
+  },
+});
 
-export default Cus_EditProfile
+export default Cus_EditProfile;
--- src/screens/Customer/Cus_Profile.js
+++ src/screens/Customer/Cus_Profile.js
@@ -14,27 +14,23 @@ import { Ionicons } from "@expo/vector-icons";
 
 import axios from "axios";
 
-
-
 const Cus_Profile = ({ navigation }) => {
-
-const[posts, setPosts] = useState([]);
-console.log("mobile prorfile")
-const id = 1;
-useEffect(() => {
-  // http://localhost:5000/api/v1/customer/getSingleCustomer/1
-  console.log("get customers called");
-  axios.get("http://10.22.163.170:5000/api/v1/customer/getSingleCustomer/"+ id)
-  .then((res) => {
-    console.log(res.data);
-    setPosts(res.data);
-    })
-    .catch ((err) => {
-    console.log(err);
-    });
-    },[]);
-
-
+  const [posts, setPosts] = useState([]);
+  console.log("mobile prorfile");
+  const id = 1;
+  useEffect(() => {
+    // http://localhost:5000/api/v1/customer/getSingleCustomer/1
+    console.log("get customers called");
+    axios
+      .get("http://10.22.163.170:5000/api/v1/customer/getSingleCustomer/" + id)
+      .then((res) => {
+        console.log(res.data);
+        setPosts(res.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
 
   return (
     <SafeAreaView
@@ -45,217 +41,208 @@ useEffect(() => {
         margin: 10,
       }}
     >
-
       <ScrollView style={{ padding: 20 }}>
         {/*---------------------------------------------------------- Profile picture section -----------------------------------------------------------------------------------*/}
-        <View
-          style={{
-            flexDirection: "column",
-            alignContent: "center",
-            alignItems: "center",
-          }}
-        >
-          <ImageBackground
-            source={require("../../assests/imgs/avatar.jpg")}
-            style={{ width: 120, height: 120 }}
-            imageStyle={{ borderRadius: 60 }}
-            
-          />
-          <View
-            style={{
-             
-              flexDirection: "row",
-              justifyContent: "center",
-              alignItems:"center",
-              paddingLeft: 30,
-            }}
-          >
+
+        {posts.map((posts) => (
+          <View>
             <View
               style={{
-               
                 flexDirection: "column",
+                alignContent: "center",
                 alignItems: "center",
-                justifyContent: "center",
-                paddingRight: 30,
-                // backgroundColor: 'black'
               }}
             >
-              <Text style={{ fontSize: 18, fontWeight: "bold" }} >
-                {posts[0].firstName+ " "+posts[0].lastName}
-              </Text>
-            
+              <ImageBackground
+                source={require("../../assests/imgs/avatar.jpg")}
+                style={{ width: 120, height: 120 }}
+                imageStyle={{ borderRadius: 60 }}
+              />
+              <View
+                style={{
+                  flexDirection: "row",
+                  justifyContent: "center",
+                  alignItems: "center",
+                  paddingLeft: 30,
+                }}
+              >
+                <View
+                  style={{
+                    flexDirection: "column",
+                    alignItems: "center",
+                    justifyContent: "center",
+                    paddingRight: 30,
+                    // backgroundColor: 'black'
+                  }}
+                >
+                  <Text
+                    style={{ fontSize: 18, fontWeight: "bold" }}
+                    key={posts.id}
+                  >
+                    {posts.firstName + " " + posts.lastName}
+                  </Text>
+                </View>
+                <View
+                  style={{
+                    justifyContent: "center",
+                    alignItems: "center",
+                  }}
+                ></View>
+              </View>
             </View>
-            <View
-              style={{
-                justifyContent: "center",
-                alignItems: "center",
-              }}
-            >
+            {/*---------------------------------------------------------- Profile picture section ---------------------------------------------------------- */}
+
+            {/*---------------------------------------------------------- Profile details heading section---------------------------------------------------------- */}
+            <View style={{ paddingTop: 20, paddingBottom: 10 }}>
+              <Text style={{ fontSize: 17, fontWeight: "bold" }}>PROFILE</Text>
+            </View>
+
+            <View>
+              <View
+                style={{
+                  borderBottomColor: "black",
+                  borderBottomWidth: 2,
+                }}
+              />
+
+              <View
+                style={{
+                  flexDirection: "row",
+                  // justifyContent: 'center',
+                  alignItems: "center",
+                  marginTop: 16,
+                  margin: 10,
+                  // backgroundColor: 'green'
+                }}
+              >
+                <ImageBackground
+                  source={require("../../assests/icons/user.png")}
+                  style={{ width: 30, height: 30 }}
+                  // imageStyle={{ borderRadius: 25 }}
+                />
+                <Text
+                  style={{
+                    paddingLeft: 20,
+                    fontSize: 16,
+                    fontWeight: "bold",
+                    width: 150,
+                  }}
+                  // key ={posts.id}
+                  key={posts.id}
+                >
+                  {posts.firstName + " " + posts.lastName}
+
+                  {/* {posts.name} */}
+                </Text>
+              </View>
+              <View
+                style={{
+                  flexDirection: "row",
+                  alignItems: "center",
+                  margin: 10,
+                }}
+              >
+                <ImageBackground
+                  source={require("../../assests/icons/icons8-call-30.png")}
+                  style={{ width: 30, height: 30 }}
+                />
+                <Text
+                  style={{
+                    paddingLeft: 20,
+                    fontSize: 16,
+                    fontWeight: "bold",
+                    width: 150,
+                  }}
+                  key={posts.id}
+                >
+                  {posts.contactNo}
+                </Text>
+              </View>
+              <View
+                style={{
+                  flexDirection: "row",
+                  alignItems: "center",
+                  margin: 10,
+                }}
+              >
+                <ImageBackground
+                  source={require("../../assests/icons/icons8-location-50.png")}
+                  style={{ width: 30, height: 30 }}
+                />
+                <Text
+                  style={{
+                    paddingLeft: 20,
+                    fontSize: 16,
+                    fontWeight: "bold",
+                    width: 250,
+                  }}
+                  key={posts.id}
+                >
+                  {posts.area + " " + posts.province}
+                </Text>
+                {/* <TextInput
+                      placeholder="Address"
+                      style={{ paddingLeft: 25, fontSize: 16, width: 150 }}
+                    /> */}
+              </View>
+              <View
+                style={{
+                  flexDirection: "row",
+                  // justifyContent: 'center',
+                  alignItems: "center",
+                  margin: 10,
+                  // backgroundColor: 'green'
+                }}
+              >
+                <ImageBackground
+                  source={require("../../assests/icons/icons8-mail-30.png")}
+                  style={{ width: 30, height: 30 }}
+                  // imageStyle={{ borderRadius: 25 }}
+                />
+                <Text
+                  style={{
+                    paddingLeft: 20,
+                    fontSize: 15,
+                    fontWeight: "bold",
+                    // width: 50,
+                  }}
+                  key={posts.id}
+                >
+                  {posts.email}
+                </Text>
+              </View>
+
+              <View
+                style={{
+                  flexDirection: "row",
+                  // justifyContent: 'center',
+                  alignItems: "center",
+                  margin: 10,
+                  // backgroundColor: 'green'
+                }}
+              >
+                <ImageBackground
+                  source={require("../../assests/icons/icons8-security-pass-24.png")}
+                  style={{ width: 30, height: 30 }}
+                  // imageStyle={{ borderRadius: 25 }}
+                />
+                <Text
+                  style={{
+                    paddingLeft: 20,
+                    fontSize: 15,
+                    fontWeight: "bold",
+                    // width: 50,
+                  }}
+                  key={posts.id}
+                >
+                  {posts.nic}
+                </Text>
+              </View>
+
+              {/* // ))} */}
             </View>
           </View>
-        </View>
-        {/*---------------------------------------------------------- Profile picture section ---------------------------------------------------------- */}
-
-        {/*---------------------------------------------------------- Profile details heading section---------------------------------------------------------- */}
-        <View style={{ paddingTop: 20, paddingBottom: 10 }}>
-          <Text style={{ fontSize: 17, fontWeight: "bold" }}>PROFILE</Text>
-        </View>
-        
-  <View>
-
-    {/*--{posts.map((posts) => ()--*/}
-
-        
-        <View
-          style={{
-            borderBottomColor: "black",
-            borderBottomWidth: 2,
-          }}
-        />
-        {/*---------------------------------------------------------- Profile details heading section ----------------------------------------------------------*/}
-
-        {/* ----------------------------------------------------------Profile details section ----------------------------------------------------------*/}
-        
-            {/*--{posts.map((posts) => ()--*/}
-
-        <View
-          style={{
-            flexDirection: "row",
-            // justifyContent: 'center',
-            alignItems: "center",
-            marginTop: 16,
-            margin:10,
-            // backgroundColor: 'green'
-          }}
-        >
-          <ImageBackground
-            source={require("../../assests/icons/user.png")}
-            style={{ width: 30, height: 30 }}
-            // imageStyle={{ borderRadius: 25 }}
-          />
-          <Text
-            style={{
-              paddingLeft: 20,
-              fontSize: 16,
-              fontWeight: "bold",
-              width: 150,
-
-            }}
-            // key ={posts.id} 
-             
-          >
-          {posts[0].firstName+ " "+posts[0].lastName}
-
-            {/* {posts.name} */}
-          </Text>
-  
-        </View>
-        <View
-          style={{
-            flexDirection: "row",
-            alignItems: "center",
-            margin: 10,
-          }}
-        >
-          <ImageBackground
-            source={require("../../assests/icons/icons8-call-30.png")}
-            style={{ width: 30, height: 30 }}
-          />
-          <Text
-            style={{
-              paddingLeft: 20,
-              fontSize: 16,
-              fontWeight: "bold",
-              width: 150,
-            }}
-          >
-      {posts[0].contactNo}
-
-          </Text>
-        </View>
-        <View
-          style={{
-            flexDirection: "row",
-            alignItems: "center",
-            margin: 10,
-          }}
-        >
-          <ImageBackground
-            source={require("../../assests/icons/icons8-location-50.png")}
-            style={{ width: 30, height: 30 }}
-          />
-          <Text
-            style={{
-              paddingLeft: 20,
-              fontSize: 16,
-              fontWeight: "bold",
-              width: 250,
-            }}
-          >
-          {posts[0].area+ " "+posts[0].province}
-          </Text>
-          {/* <TextInput
-            placeholder="Address"
-            style={{ paddingLeft: 25, fontSize: 16, width: 150 }}
-          /> */}
-        </View>
-        <View
-          style={{
-            flexDirection: "row",
-            // justifyContent: 'center',
-            alignItems: "center",
-            margin: 10,
-            // backgroundColor: 'green'
-          }}
-        >
-          <ImageBackground
-            source={require("../../assests/icons/icons8-mail-30.png")}
-            style={{ width: 30, height: 30 }}
-            // imageStyle={{ borderRadius: 25 }}
-          />
-          <Text
-            style={{
-              paddingLeft: 20,
-              fontSize: 15,
-              fontWeight: "bold",
-             // width: 50,
-            }}
-          >
-      {posts[0].email}
-          </Text>
-        </View>
-
-        <View
-          style={{
-            flexDirection: "row",
-            // justifyContent: 'center',
-            alignItems: "center",
-            margin: 10,
-            // backgroundColor: 'green'
-          }}
-        >
-          <ImageBackground
-            source={require("../../assests/icons/icons8-security-pass-24.png")}
-            style={{ width: 30, height: 30 }}
-            // imageStyle={{ borderRadius: 25 }}
-          />
-          <Text
-            style={{
-              paddingLeft: 20,
-              fontSize: 15,
-              fontWeight: "bold",
-             // width: 50,
-            }}
-          >
-            {posts[0].nic}
-
-          </Text>
-        </View>
-          
-
-
-  </View>
-
+        ))}
       </ScrollView>
       <View
         style={{
@@ -263,7 +250,10 @@ useEffect(() => {
           margin: 10,
         }}
       >
-        <CustomButton label={"Edit Profile"} onPress={() => navigation.navigate("Cus_EditProfile")}/>
+        <CustomButton
+          label={"Edit Profile"}
+          onPress={() => navigation.navigate("Cus_EditProfile")}
+        />
       </View>
     </SafeAreaView>
   );
